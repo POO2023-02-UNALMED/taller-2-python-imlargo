3cc27484ccb21114c98ef04922e76794e4b3cb25
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-08-18 10:16:36 -0500

    Test 1

---
+++ main.py
@@ -0,0 +1,52 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        colores =  [ "rojo", "verde", "amarillo", "negro", "blanco" ]
+        if (color in colores):
+            self.color = color
+
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+        
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+        
+    def asignarTipo(self, tipo):
+        if (tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo
+            
+
+class Auto:
+    cantidadCreados = 0
+    def __init__(self):
+        self.modelo
+        self.precio
+        self.asientos
+        self.marca
+        self.motor
+    
+    def cantidadAsientos(self):
+        contador = 0
+        for asiento in self.asientos:
+            if (isinstance(asiento, Asiento)):
+                contador += 1
+        return contador
+
+    def verificarIntegridad(self):
+        for asiento in self.asientos:
+            if (not isinstance(asiento, Asiento)):
+                return "Las piezas no son originales"
+
+
+        if ((self.registro) != (self.motor.registro)):
+            return "Las piezas no son originales"
+        
+        return "Auto original"
